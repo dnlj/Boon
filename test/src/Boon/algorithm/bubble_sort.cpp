8d31068c4d40159945633343770cb408047ba7cd
DanielJ <danielwjameson@yahoo.com>
2017-08-20 01:16:42 -0500

    Add multiple iterator types to the sort algorithm tests

--- test/src/Boon/algorithm/bubble_sort.cpp
+++ test/src/Boon/algorithm/bubble_sort.cpp
@@ -1,4 +1,5 @@
 // STD
+#include <list>
 #include <vector>
 
 // Catch
@@ -12,5 +13,31 @@
 
 
 TEST_CASE("Algorithm: bubble_sort", "[bubble_sort][algorithm]") {
-	SORT_ALGORITHM_TEST("Boon::bubble_sort", Boon::bubble_sort, std::vector<int>);
+	GIVEN("a bidirectional iterating container") {
+		using Container = std::list<int>;
+
+		THEN("the container iterator has the correct iterator category") {
+			using cat = std::iterator_traits<Container::iterator>::iterator_category;
+			constexpr auto same = std::is_same<cat, std::bidirectional_iterator_tag>::value;
+			REQUIRE(same);
+		}
+
+		THEN("the algorithm is correct") {
+			SORT_ALGORITHM_TEST("Boon::bubble_sort - bidirectional_iterator", Boon::bubble_sort, Container);
+		}
+	}
+
+	GIVEN("a random access iterating container") {
+		using Container = std::vector<int>;
+
+		THEN("the container iterator has the correct iterator category") {
+			using cat = std::iterator_traits<Container::iterator>::iterator_category;
+			constexpr auto same = std::is_same<cat, std::random_access_iterator_tag>::value;
+			REQUIRE(same);
+		}
+
+		THEN("the algorithm is correct") {
+			SORT_ALGORITHM_TEST("Boon::bubble_sort - random_access_iterator", Boon::bubble_sort, Container);
+		}
+	}
 }
--- test/src/Boon/algorithm/insertion_sort.cpp
+++ test/src/Boon/algorithm/insertion_sort.cpp
@@ -1,5 +1,7 @@
 // STD
 #include <forward_list>
+#include <list>
+#include <vector>
 
 // Catch
 #include <catch.hpp>
@@ -12,5 +14,45 @@
 
 
 TEST_CASE("Algorithm: insertion_sort", "[insertion_sort][algorithm]") {
-	SORT_ALGORITHM_TEST("Boon::insertion_sort", Boon::insertion_sort, std::forward_list<int>);
+	GIVEN("a forward iterating container") {
+		using Container = std::forward_list<int>;
+
+		THEN("the container iterator has the correct iterator category") {
+			using cat = std::iterator_traits<Container::iterator>::iterator_category;
+			constexpr auto same = std::is_same<cat, std::forward_iterator_tag>::value;
+			REQUIRE(same);
+		}
+
+		THEN("the algorithm is correct") {
+			SORT_ALGORITHM_TEST("Boon::insertion_sort - forward_iterator", Boon::insertion_sort, Container);
+		}
+	}
+
+	GIVEN("a bidirectional iterating container") {
+		using Container = std::list<int>;
+
+		THEN("the container iterator has the correct iterator category") {
+			using cat = std::iterator_traits<Container::iterator>::iterator_category;
+			constexpr auto same = std::is_same<cat, std::bidirectional_iterator_tag>::value;
+			REQUIRE(same);
+		}
+
+		THEN("the algorithm is correct") {
+			SORT_ALGORITHM_TEST("Boon::insertion_sort - bidirectional_iterator", Boon::insertion_sort, Container);
+		}
+	}
+
+	GIVEN("a random access iterating container") {
+		using Container = std::vector<int>;
+
+		THEN("the container iterator has the correct iterator category") {
+			using cat = std::iterator_traits<Container::iterator>::iterator_category;
+			constexpr auto same = std::is_same<cat, std::random_access_iterator_tag>::value;
+			REQUIRE(same);
+		}
+
+		THEN("the algorithm is correct") {
+			SORT_ALGORITHM_TEST("Boon::insertion_sort - random_access_iterator", Boon::insertion_sort, Container);
+		}
+	}
 }
